0b1b56498f93dbf7b15dbb3eb18b6c01dbf29189
Dutervil <dwadson3@gmail.com>
2025-03-07 15:41:12 -0500

    working on dashboard

--- pages/blogs/delete/[...id].js
+++ pages/blogs/delete/[...id].js
@@ -1,9 +1,75 @@
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {toast} from "react-toastify";
+import Head from "next/head";
+import {BsPostcard} from "react-icons/bs";
+import Blog from "@/components/Blog";
 
 
 export default function DeleteProduct() {
+    const [blogInfo, setBlogInfo] = useState(null);
 
 
+    const router = useRouter();
+    const { id } = router.query;
+    useEffect(() => {
+        if (!id) return;
+
+        axios.get('/api/blogs?id=' + id)
+            .then(response => {
+                setBlogInfo(response.data);
+            })
+            .catch(err => {
+                console.error('Axios error:', err.response ? err.response.data : err); // Log the full error details
+            });
+    }, [id]);
+
+
+    function goBack() {
+        router.push('/blogs');
+    }
+
+    async function deleteBlog() {
+        axios.delete('/api/blogs?id=' + id);
+        toast.success('Blog deleted successfully.');
+        goBack();
+    }
     return <>
-  
+      <Head>
+          <title>Delete Blog</title>
+      </Head>
+
+        <div className="blogpage">
+            <div className="titledashboard flex flex-sb">
+                <div>
+                    <h2>Delete <span>{blogInfo?.title}</span></h2>
+                    <h3>ADMIN PANEL</h3>
+                </div>
+                <div className="breadcrumb">
+                    <BsPostcard/><span>/ <span>delete blog</span></span>
+                </div>
+            </div>
+            <div className="deletesec flex flex-center wh-100">
+                 <div className="deletecard">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="60" height="60">
+                         <g fill="none" stroke="red" stroke-width="2">
+                             <path d="M3 6H21"/>
+                             <path d="M5 6V4C5 3.44772 5.44772 3 6 3H18C18.5523 3 19 3.44772 19 4V6"/>
+                             <path d="M7 6V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V6"/>
+                             <path d="M9 10V14"/>
+                             <path d="M15 10V14"/>
+                         </g>
+                     </svg>
+                     <p className="cookieHeading">Are you sure ?</p>
+                     <p className="cookieDescription">This action will permanently delete the website content</p>
+                      <div className="buttonContainer">
+                          <button onClick={deleteBlog } className="acceptButton">Delete</button>
+                          <button onClick={goBack} className="declineButton">Cancel</button>
+                      </div>
+
+                 </div>
+            </div>
+        </div>
     </>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,99 @@
 import Head from "next/head";
+import {IoHome} from "react-icons/io5";
+import {Bar} from "react-chartjs-2";
 
+import {Chart as  ChartJS, CategoryScale,LinearScale,BarElement, Title, Tooltip, Legend} from "chart.js";
+import {useEffect, useState} from "react";
 
 
 
 export default function Home() {
 
+    ChartJS.register(CategoryScale,LinearScale,BarElement, Title, Tooltip, Legend)
 
-  return (
+    const [blogData, setBlogData] = useState([]);
+    const [projectsData, setProjectsData] = useState([]);
+    const [photosData, setPhotosData] = useState([]);
+    const [shopData, setShopData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: "Blogs Created Monthly by Year",  // Title text
+            },
+        },
+    };
+
+useEffect(() => {
+   const fetchData = async () => {
+
+       try {
+           const response = await fetch("/api/blogs");
+           // const projects = await  fetch("/api/projects");
+           // const shops = await  fetch("/api/shops");
+           // const photos = await  fetch("/api/photos");
+
+           const data = await response.json();
+           // const dataProject = await projects.json();
+           // const dataPhotos = await photos.json();
+           // const dataShops = await shops.json();
+
+
+           setBlogData(data);
+           // setProjectsData(dataProject);
+           // setPhotosData(dataPhotos);
+           // setShopData(dataShops);
+           setLoading(false);
+
+       }catch(err){
+           setLoading(false);
+           console.log(err);
+       }
+   };
+   fetchData();
+},[])
+
+
+
+    const monthlyData = blogData
+        .filter(blog => blog.status === "published")
+        .reduce((acc, blog) => {
+            const blogDate = new Date(blog.createdAt.seconds * 1000); // Convert Firestore Timestamp to JS Date
+            const year = blogDate.getFullYear();
+            const month = blogDate.getMonth();
+            acc[year] = acc[year] || Array(12).fill(0);  // Corrected here
+            acc[year][month]++;  // Increment the count for the given month of the year
+
+            return acc;
+        }, {});
+
+    const  currentYear = new Date().getFullYear();
+    const years = Object.keys(monthlyData);
+    const labels =['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+    function getRandomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+    const datasets = years.map(year => ({
+             label:  `${year}`,
+            data : monthlyData[year] || Array(12).fill(0),
+           backgroundColor:getRandomColor()
+    }))
+
+    const data ={
+        labels,datasets
+    }
+    return (
 
     <>
       <Head>
@@ -14,6 +101,79 @@ export default function Home() {
         <meta name="description" content="Blog website backend" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+     <div className="dashboard">
+         <div className="titledashboard flex flex-sb">
+             <div>
+                 <h2>Admin <span>Dashboard</span> </h2>
+                 <h3>ADMIN PANEL</h3>
+             </div>
+             <div className="breadcrumb">
+                 <IoHome/> <span>/</span> <span>Dashboard</span>
+             </div>
+         </div>
+
+         {/*    Dashboard four card*/}
+         <div className="topfourcards flex flex-sb">
+             <div className="four_card">
+                 <h2>Total Blog</h2>
+                 <span>{blogData.filter(dat=> dat.status === 'published').length}</span>
+             </div>
+
+             <div className="four_card">
+                 <h2>Total Projects</h2>
+                 <span>3</span>
+             </div>
+
+             <div className="four_card">
+                 <h2>Total Products</h2>
+                 <span>2</span>
+             </div>
+             <div className="four_card">
+                 <h2>Galery Photos</h2>
+                 <span>5</span>
+             </div>
+
+         </div>
+
+        {/*    Year overview*/}
+        <div className="year_overview flex flex-sb">
+            <div className="leftyearoverview">
+             <div className=" flex flex-sb">
+                 <h3>Year Overview</h3>
+                 <ul className="creative-dots">
+                     <li className="big-dot"></li>
+                     <li className="semi-big-dot"></li>
+                     <li className="medium-dot"></li>
+                     <li className="semi-medium-dot"></li>
+                     <li className="semi-small-dot"></li>
+                     <li className="small-dot"></li>
+                 </ul>
+                 <h3>{blogData.filter(dat=> dat.status === 'published').length}/365 <br/> <span>Total published</span></h3>
+             </div>
+                <Bar options={options}  data={data}/>
+            </div>
+            <div className="right_salescont">
+                <div>
+                    <h3>Blogs By Category</h3>
+                    <ul className="creative-dots">
+                        <li className="big-dot"></li>
+                        <li className="semi-big-dot"></li>
+                        <li className="medium-dot"></li>
+                        <li className="semi-medium-dot"></li>
+                        <li className="semi-small-dot"></li>
+                        <li className="small-dot"></li>
+                    </ul>
+                </div>
+
+                <div className="blog">
+
+
+                </div>
+            </div>
+        </div>
+
+     </div>
     </>
 
   );
